febf8e9cc55c8ee3336d53b1dcb3210db697f972
YouMuCZ <87563407+YouMuCZ@users.noreply.github.com>
2023-10-10 00:25:21 +0800

    Update tile map and player(camera, move with mouse)

---
+++ 2d/brotato/README.md
@@ -0,0 +1,9 @@
+# brotato
+
+《土豆兄弟》复刻作品,用以学习godot和基础的游戏制作。
+教程来源：https://space.bilibili.com/13496908/channel/collectiondetail?sid=1727298
+
+# 开发日志
+
+## 2023-10-10
+-
---
+++ 2d/brotato/player/player.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+
+var direc = Vector2.ZERO
+var speed = 400
+var canMove = true
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _physics_process(delta: float) -> void:
+	var mouse_pos = get_global_mouse_position()
+	var self_pos = position
+
+	direc = (mouse_pos - self_pos).normalized()
+
+	if canMove:
+		velocity = direc * speed
+		move_and_slide()
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+		canMove = false
+
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and not event.is_pressed():
+		canMove = true
